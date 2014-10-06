a0522f430570a56afc2a351f609ae0d4729e58fb
Jack Weeden <jack@ajack.org>
2014-10-06 10:27:12 +0100

    cherry-picked commit e5489d0 from 'spree_auth_devise/master' (added confirmable module)

--- app/mailers/spree/user_mailer.rb
+++ app/mailers/spree/user_mailer.rb
@@ -7,5 +7,11 @@ module Spree
           :subject => Spree::Config[:site_name] + ' ' +
             I18n.t(:subject, :scope => [:devise, :mailer, :reset_password_instructions]))
     end
+
+    def confirmation_instructions(user, token, opts={})
+      @confirmation_url = spree.spree_user_confirmation_url(:confirmation_token => token, :host => Spree::Store.current.url)
+
+      mail to: user.email, from: from_address, subject: Spree::Store.current.name + ' ' + I18n.t(:subject, :scope => [:devise, :mailer, :confirmation_instructions])
+    end
   end
 end
--- app/models/spree/user.rb
+++ app/models/spree/user.rb
@@ -7,6 +7,7 @@ module Spree
 
     devise :database_authenticatable, :registerable, :recoverable,
            :rememberable, :trackable, :validatable, :encryptable, :encryptor => 'authlogic_sha512'
+    devise :confirmable if Spree::Auth::Config[:confirmable]
 
     has_many :orders
 
@@ -42,6 +43,14 @@ module Spree
       def set_login
         # for now force login to be same as email, eventually we will make this configurable, etc.
         self.login ||= self.email if self.email
+      end
+
+      def scramble_email_and_password
+        self.email = SecureRandom.uuid + "@example.net"
+        self.login = self.email
+        self.password = SecureRandom.hex(8)
+        self.password_confirmation = self.password
+        self.save
       end
   end
 end
