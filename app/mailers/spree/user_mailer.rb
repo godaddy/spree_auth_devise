648c24d5f9e7baeead526c7c75327da59f08206a
Justin Lai <jlai@godaddy.com>
2015-08-05 17:05:15 -0700

    don't send any emails if site_url is not set

--- app/mailers/spree/user_mailer.rb
+++ app/mailers/spree/user_mailer.rb
@@ -1,21 +1,27 @@
 module Spree
   class UserMailer < BaseMailer
     def reset_password_instructions(user, token, *args)
-      @edit_password_reset_url = spree.edit_spree_user_password_url(:reset_password_token => token, :host => Spree::Config.site_url)
+      if Spree::Config.site_url
+        @edit_password_reset_url = spree.edit_spree_user_password_url(:reset_password_token => token, :host => Spree::Config.site_url)
 
-      mail(:to => user.email, :from => from_address,
-          :subject => Spree::Config[:site_name] + ' ' +
-            I18n.t(:subject, :scope => [:devise, :mailer, :reset_password_instructions]))
+        mail(:to => user.email, :from => from_address,
+             :subject => Spree::Config[:site_name] + ' ' +
+                 I18n.t(:subject, :scope => [:devise, :mailer, :reset_password_instructions]))
+      end
     end
 
     def confirmation_instructions(user, token, opts={})
       #hack so that confirmable module is still used after first confirmation
       Spree::Auth::Config[:confirmable] = true
-      @user = user
-      host = Spree::Config.site_url.present? ? Spree::Config.site_url : "no_site_url"
-      @confirmation_url = spree.spree_user_confirmation_url(:confirmation_token => token, :host => host)
-      @email = @user.pending_reconfirmation? ? @user.unconfirmed_email : @user.email
-      mail to: @email, from: from_address, subject: Spree::Config.site_name + ' ' + I18n.t(:subject, :scope => [:devise, :mailer, :confirmation_instructions])
+      if Spree::Config.site_url
+        @user = user
+        @confirmation_url = spree.spree_user_confirmation_url(:confirmation_token => token, :host => Spree::Config.site_url)
+        @email = @user.pending_reconfirmation? ? @user.unconfirmed_email : @user.email
+
+        mail to: @email, from: from_address,
+             subject: Spree::Config.site_name + ' ' +
+                 I18n.t(:subject, :scope => [:devise, :mailer, :confirmation_instructions])
+      end
     end
   end
 end
