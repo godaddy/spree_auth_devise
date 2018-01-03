feb196b4fd7552c62b106fa367e3cdc88e360d63
Karl Smith <kesmith@godaddy.com>
2018-01-03 15:25:53 -0700

    fix rspec deprecations

--- spec/controllers/spree/user_registrations_controller_spec.rb
+++ spec/controllers/spree/user_registrations_controller_spec.rb
@@ -4,7 +4,7 @@ describe Spree::UserRegistrationsController do
     @request.env["devise.mapping"] = Devise.mappings[:spree_user]
   end
   context '#create' do
-    before { controller.stub(:after_sign_up_path_for).and_return(spree.root_path(thing: 7)) }
+    before { allow(controller).to receive(:after_sign_up_path_for).and_return(spree.root_path(thing: 7)) }
     it 'should redirect to after_sign_up_path_for' do
       spree_post :create, { :spree_user => { :email => 'foobar@example.com', :password => 'foobar123', :password_confirmation => 'foobar123' } }
       expect(response).to redirect_to spree.root_path(thing: 7)
